7fdaf12b7bc082b252fcacc67049e7564ca3a1b6
saleh-havid <saleh.havid@gmail.com>
2021-05-02 01:26:21 +0700

    destructure homepage to smaller component and finish styling

---
+++ src/pages/homepages/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+import "../../components/directory/directory.components";
+import Directory from "../../components/directory/directory.components";
+
+const HomePage = () => {
+  return <Directory />;
+};
+
+export default HomePage;
